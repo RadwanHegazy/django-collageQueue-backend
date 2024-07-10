91c08a518e50c1f51b50630e07949a8efabfeab1
RadwanHegazy <radwangaber22@gmail.com>
2024-07-10 10:06:59 +0300

    finsh backend

--- exam/apis/urls.py
+++ exam/apis/urls.py
@@ -1,7 +1,9 @@
-from .views import get
+from .views import get, patch
 from django.urls import path
 
 urlpatterns = [
     path('get/',get.home_view.as_view(), name='home'),
     path('get/<int:exam_id>/',get.exam_view.as_view(), name='exam_view'),
+    path('section/pending/<int:section_id>/',patch.section_pending.as_view(), name='section_pending'),
+    path('section/done/<int:section_id>/',patch.section_done.as_view(), name='section_done'),
 ]
